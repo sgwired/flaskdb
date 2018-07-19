1d5ef8cda9eb9e8922803678506a2b4d0d0835bf
Shelton Gayles <shelton@sheltons-mini.lan>
2018-07-18 18:48:33 -0700

    added mysql database

--- guestbook.py
+++ guestbook.py
@@ -1,12 +1,27 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
-
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://query:query@127.0.0.1/test'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+db = SQLAlchemy(app)
+
+class Comments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    comment = db.Column(db.String(1000))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    results = Comments.query.all()
+    return render_template('index.html', results=results)
 
 @app.route('/sign')
 def sign():
@@ -16,7 +31,11 @@ def sign():
 def process():
     name = request.form['name']
     comment = request.form['comment']
-    return render_template('index.html', name=name, comment=comment)
+
+    signature = Comments(name=name, comment=comment)
+    db.session.add(signature)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 @app.route('/home/<place>')
 def home(place):
