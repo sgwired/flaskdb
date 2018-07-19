bb9f6097f44662fb36cc55928645b374dd51ae06
Shelton Gayles <sgbythelake@gmail.com>
2018-07-18 19:20:31 -0700

    added sqlachemy db connection

---
+++ app.py
@@ -0,0 +1,16 @@
+#https://github.com/PyMySQL/mysqlclient-python
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config.from_pyfile('config.cfg')
+
+db = SQLAlchemy(app)
+
+
+class Test(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+if __name__ == '__main__':
+    app.run()
